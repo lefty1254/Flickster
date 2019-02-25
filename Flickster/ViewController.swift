3212e8f0fbc097a878ce082152bc19d8fc4bfd19
Eleftherios Troullouris <Lefteris@dimitriss-macbook-air.local>
2019-02-25 14:24:15 -0500

    Initial Commit

---
+++ Flickster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flickster
+//
+//  Created by Eleftherios Troullouris on 2/25/19.
+//  Copyright © 2019 troullouris@hotmail.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
